05817a9468e81750a9835d908259b05a0bfc7df2
Gleyson Sampaio <gleyson.s@hotmail.com>
2018-03-14 14:07:31 -0300

    testando o banco

--- cfip-se-rascunho/src/main/java/edu/cfip/database/Dao.java
+++ cfip-se-rascunho/src/main/java/edu/cfip/database/Dao.java
@@ -3,13 +3,24 @@ package edu.cfip.database;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
+import edu.cfip.model.Conta;
+
 public class Dao {
 	private EntityManager em;
 	public Dao() {
 		em=Persistence.createEntityManagerFactory("PU_CFIP").createEntityManager();
 		System.out.println("EntityManager criado");
 	}
+	public void incluir(Object entidade) {
+		em.getTransaction().begin();
+		em.persist(entidade);
+		em.getTransaction().commit();
+	}
 	public static void main(String[] args) {
-		new Dao();
+		Dao d = new Dao();
+		Conta c = new Conta();
+		c.setNome("TESTE");
+		d.incluir(c);
+		System.out.println("Salvou");
 	}
 }
