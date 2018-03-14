beaf3b39c8a0d46721ef9b7de92d6d0fb2935ee5
Porgamador <porgamador@gmail.com>
2018-03-14 05:57:08 -0300

    primeira apresentacao

---
+++ cfip-se/src/main/java/edu/cfip/util/desktop/Sessao.java
@@ -0,0 +1,41 @@
+package edu.cfip.util.desktop;
+
+public class Sessao {
+	private Integer usuario;
+	private String login;
+	private String nome;
+	private Integer empresa;
+	private String nomeFantasia;
+	public Integer getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Integer usuario) {
+		this.usuario = usuario;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(Integer empresa) {
+		this.empresa = empresa;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	
+	
+}
